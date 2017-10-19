379870d79a8ac6916921da16e066156f5294538f
AdrienChampion <adrien.champion@email.com>
2017-10-20 05:47:47 +0900

    [travis] rmed opam init

--- .travis.sh
+++ .travis.sh
@@ -1,6 +1,5 @@
 wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s .
 
-./opam -y init --comp=4.05.0
 eval `./opam config env`
 ./opam -y install omake menhir core re2 ppx_deriving ppx_sexp_conv ppx_hash ppx_variants_conv ppx_fields_conv ppx_compare ppx_driver
 eval `./opam config env`
