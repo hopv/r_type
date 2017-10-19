6f88ccfcccfcf602e38ca7df2d6f9da5c51efeb2
AdrienChampion <adrien.champion@email.com>
2017-10-20 05:25:02 +0900

    trying new travis tweaks

--- .travis.sh
+++ .travis.sh
@@ -1,11 +1,11 @@
-wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
+wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s .
 
-opam init --comp=4.05.0
-eval `opam config env`
-opam install omake menhir core re2 ppx_deriving ppx_sexp_conv ppx_hash ppx_variants_conv ppx_fields_conv ppx_compare ppx_driver
-eval `opam config env`
+./opam init --comp=4.05.0
+eval `./opam config env`
+./opam install omake menhir core re2 ppx_deriving ppx_sexp_conv ppx_hash ppx_variants_conv ppx_fields_conv ppx_compare ppx_driver
+eval `./opam config env`
 
-opam -version
+./opam -version
 ocaml -version
 
 omake
