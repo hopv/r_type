3472fb847d1ca31205dc354563bc93bbfa222150
AdrienChampion <adrien.champion@email.com>
2017-10-20 05:33:14 +0900

    added confirmation in travis script

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,8 @@
 wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s .
 
-./opam init --comp=4.05.0
+yes | ./opam init --comp=4.05.0
 eval `./opam config env`
-./opam install omake menhir core re2 ppx_deriving ppx_sexp_conv ppx_hash ppx_variants_conv ppx_fields_conv ppx_compare ppx_driver
+yes | ./opam install omake menhir core re2 ppx_deriving ppx_sexp_conv ppx_hash ppx_variants_conv ppx_fields_conv ppx_compare ppx_driver
 eval `./opam config env`
 
 ./opam -version
