28cd640916123ddddef42646fb580237da0b7bac
AdrienChampion <adrien.champion@email.com>
2017-12-27 20:14:28 +0900

    dunno what to do

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,7 @@
 wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s .
 
 eval `./opam config env`
-./opam install -y omake.0.10.2 menhir core re2 ppx_deriving ppx_sexp_conv ppx_hash ppx_variants_conv ppx_fields_conv ppx_compare ppx_driver
+./opam install -y omake.0.10.3 menhir core re2 ppx_deriving ppx_sexp_conv ppx_hash ppx_variants_conv ppx_fields_conv ppx_compare ppx_driver
 eval `./opam config env`
 
 omake
