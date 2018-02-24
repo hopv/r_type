416a14a300755dfee2c87ca587a67a72436483f4
AdrienChampion <adrien.champion@email.com>
2018-02-24 14:14:59 +0900

    still trying to fix build

--- .travis.sh
+++ .travis.sh
@@ -3,7 +3,7 @@
 wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s .
 
 eval `./opam config env`
-./opam install -y omake.0.10.2 menhir core.v0.9.1 re2.v0.9.0 ppx_deriving.4.2 ppx_sexp_conv.v0.9.0 ppx_hash.v0.9.0 ppx_variants_conv.v0.9.0 ppx_fields_conv.v0.9.0 ppx_compare.v0.9.0 ppx_driver.v0.9.0
+./opam install -y omake.0.10.2 menhir core.v0.9.1 jbuilder.1.0+beta14 re2.v0.9.0 ppx_deriving.4.2 ppx_sexp_conv.v0.9.0 ppx_hash.v0.9.0 ppx_variants_conv.v0.9.0 ppx_fields_conv.v0.9.0 ppx_compare.v0.9.0 ppx_driver.v0.9.0
 eval `./opam config env`
 
 ./opam --version
